e63f4bca15b63bd01ebb40e31bf4e71cf37e8d9d
cmloof <cmlooff@gmail.com>
2022-12-01 15:21:20 -0500

    No update

--- 2022/Rust/day-01/src/bin/part-1.rs
+++ 2022/Rust/day-01/src/bin/part-1.rs
@@ -1,19 +1,5 @@
 use day_01::process_part1;
-use std::fs; // Using std::fs to read the input.txt into a string
-/** Elves Calories
- * First Elf:   1000, 2000, 3000 =  6000
- * Second Elf:  4000             =  4000
- * Third Elf:   5000, 6000       =  11,000
- * Fourth Elf:  7000, 8000, 9000 =  24,000
- * Fifth Elf:   10,000           =  10,000
- */
-
-/** Problem Statement
- * In case the Elves get hungry and need extra snacks, they need to know which Elf to ask:
- * they'd like to know how many Calories are being carried by the Elf carrying the most Calories.
- * In the example above, this is 24000 (carried by the fourth Elf).
- * Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
- */
+use std::fs;
 
 fn main() {
     let file = fs::read_to_string("./input.txt").unwrap();
--- 2022/Rust/day-01/src/lib.rs
+++ 2022/Rust/day-01/src/lib.rs
@@ -1,16 +1,30 @@
+// pub fn process_part1(input: &str) -> String {
+//     // Chunked up the input by new lines
+//     let result = input
+//         .split("\n\n")
+//         .map(|elf_load| {
+//             // elf_load is the chunk of the listed numbers
+//             elf_load
+//                 .lines() // On new line
+//                 .map(|item| item.parse::<u32>().unwrap()) // Mapping each line, parsing each. -> ::<u32> turbofish syntax. Telling parse to return a u32 -> Then we have to unwrap that
+//                 .sum::<u32>()
+//         }) // We are summing the values during this map
+//         .max() // Returns a result itself
+//         .unwrap(); // Unwrapping that result
+//     result.to_string()
+// }
+
 pub fn process_part1(input: &str) -> String {
-    // Chunked up the input by new lines
     let result = input
         .split("\n\n")
         .map(|elf_load| {
-            // elf_load is the chunk of the listed numbers
             elf_load
-                .lines() // On new line
-                .map(|item| item.parse::<u32>().unwrap()) // Mapping each line, parsing each. -> ::<u32> turbofish syntax. Telling parse to return a u32 -> Then we have to unwrap that
+                .lines()
+                .map(|item| item.parse::<u32>().unwrap())
                 .sum::<u32>()
-        }) // We are summing the values during this map
-        .max() // Returns a result itself
-        .unwrap(); // Unwrapping that result
+        })
+        .max()
+        .unwrap();
     result.to_string()
 }
 
@@ -20,9 +34,7 @@ mod tests {
     // Module tests
     use super::*;
 
-    #[test] // Test Macro
-    fn it_works() {
-        let input: &str = "1000
+    const INPUT: &str = "1000
 2000
 3000
 
@@ -36,7 +48,10 @@ mod tests {
 9000
 
 10000";
-        let result = process_part1(input);
+
+    #[test] // Test Macro
+    fn it_works() {
+        let result = process_part1(INPUT);
         assert_eq!(result, "24000");
     }
 }
